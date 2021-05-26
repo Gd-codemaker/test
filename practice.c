3905ae3e453cbecd0bc94366e19434d5cc7c2502
Pachai <suryapachai08@gmail.com>
2021-05-26 13:03:39 +0530

    Changes

--- practice.c
+++ practice.c
@@ -4,4 +4,5 @@ int main()
 	 printf("I am got good name");
 	 printf("Hii i am going to make some changes in programming.\n");
 	 printf("Hii once again I am going to changes my code:\n");
+	 printf("This is my changes\n");
 }
